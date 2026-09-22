a0d4c9a5ef892a77b76376aa795447a8d45fde36
FernandoDelgadoDiaz <fernandogastondelgado81@gmail.com>
2026-09-22 16:43:16 -0300

    refactor: consolidate discovery into single controller

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { autonomyRouter } from './routes/autonomy.js';
 import { discoveryRouter } from './routes/discovery.js';
 
 export const app = express();
-export const BUILD_VERSION = '0.9.5-discovery-direct-read';
+export const BUILD_VERSION = '0.9.6-discovery-single-controller';
 
 app.use(express.json({ limit: '1mb' }));
 app.use(express.text({ type: 'text/plain', limit: '1mb' }));
