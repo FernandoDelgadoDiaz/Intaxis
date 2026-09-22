26c3fba98eb997125de43b37d5a2319b44b4d235
FernandoDelgadoDiaz <fernandogastondelgado81@gmail.com>
2026-09-22 14:34:01 -0300

    chore: bump one-tap discovery build

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { autonomyRouter } from './routes/autonomy.js';
 import { discoveryRouter } from './routes/discovery.js';
 
 export const app = express();
-export const BUILD_VERSION = '0.9.0-generic-pyme-profile';
+export const BUILD_VERSION = '0.9.1-discovery-one-tap';
 
 app.use(express.json({ limit: '1mb' }));
 app.use(express.text({ type: 'text/plain', limit: '1mb' }));
